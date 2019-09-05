6f2b6844749d5295aa8d9b1abe1685ee3a10d5c7
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 20:38:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,19 @@
 def turn(board)
   puts "Please enter number 1-9:"
+  input = gets.strip
+
+
 end
 
-def display_board(board, index)
- board(0) | board(1) | board(2)
------------
- board(3) | board(4) | board(5)
------------
- board(6) | board(7) | board(8) 
+def display_board(board)
+puts " #{board(0)} | #{board(1)} | #{board(2)} "
+puts "-----------"
+puts " #{board(3)} | #{board(4)} | #{board(5)} "
+puts "-----------"
+puts " #{board(6)} | #{board(7)} | #{board(8)} "
+end
+
+def input_to_index(input)
+  input = input.to_i
+  input += -1
+end
