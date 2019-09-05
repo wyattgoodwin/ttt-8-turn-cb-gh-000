b671719bdb474240692526751250041ec4522143
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 20:44:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,3 +17,23 @@ def input_to_index(input)
   input = input.to_i
   input += -1
 end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0, 8)
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
