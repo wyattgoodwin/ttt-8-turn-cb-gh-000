758edf6e7fdbbdee8989882c80ef1bd30b219737
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 20:33:09 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+def turn(board)
+  puts "Please enter number 1-9:"
+end
+
+def display_board(board, index)
+ board(0) | board(1) | board(2)
+-----------
+ board(3) | board(4) | board(5)
+-----------
+ board(6) | board(7) | board(8) 
